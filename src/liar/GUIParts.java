38fc25113b64fbfb725660edb16c83097bdbe93a
UMA <blog3170@gmail.com>
2019-12-16 14:38:35 +0900

    Update GUIParts.java

--- src/liar/GUIParts.java
+++ src/liar/GUIParts.java
@@ -10,5 +10,59 @@ package liar;
  * @author f1610801320
  */
 public class GUIParts {
+    protected void initButton() {
+        
+    }
     
+    protected void initComboBox() {
+        
+    }
+    
+    protected void initImage() {
+        
+    }
+    
+    protected void initImageView() {
+        
+    }
+    
+    protected void initLabel() {
+        
+    }
+    
+    protected void GUIParts() {
+        
+    }
+    
+    protected static void setCardDropDownForm() {
+        
+    }
+    
+    protected static void setTimerLable() {
+        
+    }
+    
+    protected static void setFieldCardLabel() {
+        
+    }
+    
+    protected static void setThemeLabel() {
+        
+    }
+    
+    protected static void setDoubtTitleLabel() {
+        
+    }
+
+    protected static void setDoubtResultLabel() {
+        
+    }
+    
+    protected static void setMessageLabel() {
+        
+    }
+    
+    protected static void setCurrentNumberOfLabel() {
+        
+    }
 }
