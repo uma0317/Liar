c55de33dc702619d8bd212ee335a4d8dca1753c9
UMA <blog3170@gmail.com>
2019-12-16 15:18:17 +0900

    プロパティとメソッド全部追加した

--- src/liar/GUIParts.java
+++ src/liar/GUIParts.java
@@ -5,11 +5,50 @@
  */
 package liar;
 
+import java.util.ArrayList;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  *
  * @author f1610801320
  */
 public class GUIParts {
+    Image[][] iconImage;
+    ImageView[][] iconImageView;
+    Label playerDispLabel1, playerDispLabel2;
+    
+    //カード配布
+    Button gameStartBtn;
+    Image imageRight, imageLeft;
+    ImageView imageRightView, imageLeftView;
+    Label playerFormMessageLabel, titleLabel, subTitleLabel;
+    ComboBox<Integer> inputPlayerNumForm;
+    
+    //ゲーム終了
+    Image finalResultImageRight, finalResultImageLeft;
+    ImageView finalResultImageRightView, finalResultImageLeftView;
+    Label finalResultTitleLabel;
+    
+    //カード選択
+    Image fieldCardsFrameImage;
+    ImageView currentPlayerView, fieldCardsFrameImageView;
+    Button startButton, desicionButton;
+    ArrayList<ComboBox<Integer>> cardDropDownForm;
+    Label fieldCardLabel, themeCardLabel;
+    Label[] patternLabel, currentNumberOfCardList;
+    
+    //ダウト確認
+    Image themeFrameImage, doubtButtonImage;
+    ImageView doubtPlayerView, themeFrameImageView;
+    Label timerLabel, numberOfCardLabel;
+    
+    //ダウト結果
+    Label doubtTitleLabel, doubtResultLabel, messageLabel;
+    
     protected void initButton() {
         
     }
@@ -34,35 +73,35 @@ public class GUIParts {
         
     }
     
-    protected static void setCardDropDownForm() {
+    protected static void setCardDropDownForm(int[] divideCardData) {
         
     }
     
-    protected static void setTimerLable() {
+    protected static void setTimerLable(int time) {
         
     }
     
-    protected static void setFieldCardLabel() {
+    protected static void setFieldCardLabel(int num) {
         
     }
     
-    protected static void setThemeLabel() {
+    protected static void setThemeLabel(int theme) {
         
     }
     
-    protected static void setDoubtTitleLabel() {
+    protected static void setDoubtTitleLabel(boolean judge) {
         
     }
 
-    protected static void setDoubtResultLabel() {
+    protected static void setDoubtResultLabel(boolean judge, int theme, int garbage) {
         
     }
     
-    protected static void setMessageLabel() {
+    protected static void setMessageLabel(boolean judge) {
         
     }
     
-    protected static void setCurrentNumberOfLabel() {
+    protected static void setCurrentNumberOfLabel(int cardDataNum) {
         
     }
 }
