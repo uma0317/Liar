d44cb5f4bb641bd246b6f2f9f5bec37b7719a41a
UMA <blog3170@gmail.com>
2019-12-16 15:01:35 +0900

    Update SceneControl.java

--- src/liar/SceneControl.java
+++ src/liar/SceneControl.java
@@ -5,10 +5,50 @@
  */
 package liar;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author f1610801320
  */
 public class SceneControl {
+    private InputControl input;
+    static Scene gameStartScene, gameEndScene, playerDispScene, cardChoiseScene, doubtCheckScene, doubtPlayerScene, doubtResultScene;
     
+    SceneControl() {
+        
+    }
+    
+    static void transitionScene(Stage stage, Stage changeScene) {
+        
+    }
+    
+    private void setGameStartScene(Stage primaryStage) {
+        
+    }
+    
+    private void setGameEndScene(Stage primaryStage) {
+        
+    }
+    
+    private void setPlayerDispScene(Stage primaryStage) {
+        
+    }
+    
+    private void setCardChoiseScene(Stage primaryStage) {
+        
+    }
+    
+    private void setDoubtCheckScene(Stage primaryStage) {
+        
+    }
+    
+    private void setDoubtPlayerCheckScene(Stage secondaryStage) {
+        
+    }
+    
+    private void setDoubtResultScene(Stage primaryStage) {
+        
+    }
 }
