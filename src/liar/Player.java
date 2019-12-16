8da6271277bff82d11a8eccf6cc129c92fdbdd5c
imaisoraki <58286302+imaisoraki@users.noreply.github.com>
2019-12-16 16:20:26 +0900

    12/16作成分

--- src/liar/Player.java
+++ src/liar/Player.java
@@ -10,5 +10,34 @@ package liar;
  * @author f1610801320
  */
 public class Player {
-    
+    imageView[] iconimage;
+    ArrayList<Card> cardData;
+    public void deleteCardData(Card cards[]){
+        n=0;
+        m=0;
+        while(n<cards.length){
+            Card card = cards[n];
+            while(m<cardData.length){
+                if(Card.getNumber() == cardData[m].getNumber()){
+                    
+                }
+            }
+        }
+            
+    }
+    public void setCardData(){
+        n=0;
+        while(n<cards.length){
+            cardData.add(cards[n])
+        }
+    }
+    public ArrayList<Card> getCardData(){
+        return cardData;
+    }
+    public imageView[] getIconImage(int expression){
+        return iconImage[expression];
+    }
+    public void Player(String ){
+        
+    }
 }
